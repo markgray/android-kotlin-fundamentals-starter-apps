f40683119909b357f6dd584df2334661f14a7ec9
markgray <mark.t.gray@gmail.com>
2020-01-02 06:56:34 -0500

    Added LiveData transformation to GuessTheWord

--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,8 +1,11 @@
 package com.example.android.guesstheword.screens.game
 
+import android.os.CountDownTimer
+import android.text.format.DateUtils
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 /**
@@ -37,13 +40,34 @@ class GameViewModel : ViewModel() {
         get() = _eventGameFinish
 
     /**
+     * Countdown time
+     */
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
+
+    /**
+     * The String version of the current time, its value is set by the [Transformations.map] method
+     * with a lambda which converts our [currentTime] field to a [String]
+     */
+    val currentTimeString = Transformations.map(currentTime) { time ->
+        DateUtils.formatElapsedTime(time)
+    }
+
+    private val timer: CountDownTimer
+
+    /**
      * We set the value of our `MutableLiveData` field `_word` to the empty string (it is
      * the backing field for our `LiveData` field `word`), and the value of our
      * `MutableLiveData` field `_score` to 0 (it is the backing field for our `LiveData`
      * field `score`). We then call our `resetList` method to initialize our
      * `MutableList<String>` field `wordList` to a shuffled List of words to guess, then call
      * our `nextWord` method to initialize our `word` field to the zeroth entry in `wordList`
-     * and remove that word from the list.
+     * and remove that word from the list. We then initialize our `CountDownTimer` field `timer`
+     * to a new instance which will countdown from COUNTDOWN_TIME milliseconds to 0 using an
+     * interval of ONE_SECOND milliseconds and override its `onTick` callback to update the value
+     * of our `MutableLiveData<Long>` field `_currentTime`, and its `onFinish` callback to call
+     * our method `onGameFinish`
      */
     init {
         _word.value = ""
@@ -51,17 +75,34 @@ class GameViewModel : ViewModel() {
         resetList()
         nextWord()
         Log.i("GameViewModel", "GameViewModel created!")
+
+        // Creates a timer which triggers the end of the game when it finishes
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                _currentTime.value = millisUntilFinished/ONE_SECOND
+            }
+
+            override fun onFinish() {
+                _currentTime.value = DONE
+                onGameFinish()
+            }
+        }
+
+        timer.start()
     }
 
     /**
      * This method will be called when this [ViewModel] is no longer used and will be
      * destroyed. It is useful when [ViewModel] observes some data and you need to clear
-     * this subscription to prevent a leak of this [ViewModel]. We just call our super's
-     * implementation of `onCleared`.
+     * this subscription to prevent a leak of this [ViewModel]. We call our super's
+     * implementation of `onCleared` then call the `cancel` method of our [CountDownTimer]
+     * field [timer] to cancel the countdown and free its resources.
      */
     override fun onCleared() {
         super.onCleared()
-        Log.i("GameViewModel", "GameViewModel destroyed!")
+        // Cancel the timer
+        timer.cancel()
     }
 
     /**
@@ -97,16 +138,17 @@ class GameViewModel : ViewModel() {
     /**
      * Moves to the next word in the list and removes it from the list. If our
      * `MutableList<String>` field [wordList] is empty (all words have been guessed at)
-     * we call our method [onGameFinish]. Otherwise we set the value of our
+     * we call our method [resetList] to refill the list. Otherwise we set the value of our
      * `MutableLiveData<String>` field [_word] (private backing field of our public
      * `LiveData<String>` field [word]) to the zeroth entry in our `MutableList<String>`
      * field [wordList] removing that word from the list.
      */
-    fun nextWord() {
+    private fun nextWord() {
+        // Shuffle the word list, if the list is empty
         if (wordList.isEmpty()) {
-            onGameFinish()
+            resetList()
         } else {
-            //Select and remove a _word from the list
+            // Remove a word from the list
             _word.value = wordList.removeAt(0)
         }
     }
@@ -157,5 +199,27 @@ class GameViewModel : ViewModel() {
      */
     fun onGameFinishComplete() {
         _eventGameFinish.value = false
+    }
+
+    /**
+     * Our static constants
+     */
+    companion object {
+
+        /**
+         * Time when the game is over
+         */
+        private const val DONE = 0L
+
+        /**
+         * Countdown time interval
+         */
+        private const val ONE_SECOND = 1000L
+
+        /**
+         * Total time for the game
+         */
+        private const val COUNTDOWN_TIME = 10000L
+
     }
 }
