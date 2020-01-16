8b9c5cbffca8ccec8d56ac0df9fb20f26b16781e
markgray <mark.t.gray@gmail.com>
2020-01-16 06:51:48 -0500

    Added navigation to DetailFragment to MarsRealEstate-Starter

--- MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -21,11 +21,14 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.android.marsrealestate.network.MarsApi
+import com.example.android.marsrealestate.network.MarsApiFilter
 import com.example.android.marsrealestate.network.MarsProperty
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+
+enum class MarsApiStatus { LOADING, ERROR, DONE }
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
@@ -33,11 +36,12 @@ import kotlinx.coroutines.launch
 class OverviewViewModel : ViewModel() {
 
     // The internal MutableLiveData String that stores the most recent response
-    private val _response = MutableLiveData<String>()
+    private val _status = MutableLiveData<MarsApiStatus>()
 
     // The external immutable LiveData for the response String
-    val response: LiveData<String>
-        get() = _response
+    @Suppress("unused")
+    val status: LiveData<MarsApiStatus>
+        get() = _status
 
     // The internal MutableLiveData MarsProperty that stores the properites from the response
     private val _properties = MutableLiveData<List<MarsProperty>>()
@@ -45,6 +49,10 @@ class OverviewViewModel : ViewModel() {
     // The external immutable LiveData MarsProperty that stores the properites from the response
     val properties: LiveData<List<MarsProperty>>
         get() = _properties
+
+    private val _navigateToSelectedProperty = MutableLiveData<MarsProperty>()
+    val navigateToSelectedProperty: LiveData<MarsProperty>
+        get() = _navigateToSelectedProperty
 
     private var viewModelJob = Job()
 
@@ -55,23 +63,37 @@ class OverviewViewModel : ViewModel() {
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
      */
     init {
-        getMarsRealEstateProperties()
+        getMarsRealEstateProperties(MarsApiFilter.SHOW_ALL)
     }
 
     /**
      * Sets the value of the status LiveData to the Mars API status.
      */
-    private fun getMarsRealEstateProperties() {
+    private fun getMarsRealEstateProperties(filter: MarsApiFilter) {
         coroutineScope.launch {
-            val getPropertiesDeferred = MarsApi.retrofitService.getProperties()
+            var getPropertiesDeferred = MarsApi.retrofitService.getProperties(filter.value)
             try {
-                val listResult = getPropertiesDeferred.await()
-                _response.value = "Success: ${listResult.size} Mars properties retrieved"
+                _status.value = MarsApiStatus.LOADING
+                var listResult = getPropertiesDeferred.await()
+                _status.value = MarsApiStatus.DONE
                 _properties.value = listResult
             } catch (e: Exception) {
-                _response.value = "Failure: ${e.message}"
+                _status.value = MarsApiStatus.ERROR
+                _properties.value = ArrayList()
             }
         }
+    }
+
+    fun updateFilter(filter: MarsApiFilter) {
+        getMarsRealEstateProperties(filter)
+    }
+
+    fun displayPropertyDetails(marsProperty: MarsProperty) {
+        _navigateToSelectedProperty.value = marsProperty
+    }
+
+    fun displayPropertyDetailsComplete() {
+        _navigateToSelectedProperty.value = null
     }
 
     override fun onCleared() {
--- MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/overview/PhotoGridAdapter.kt
+++ MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/overview/PhotoGridAdapter.kt
@@ -25,51 +25,25 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.android.marsrealestate.databinding.GridViewItemBinding
 import com.example.android.marsrealestate.network.MarsProperty
 
-class PhotoGridAdapter : ListAdapter<MarsProperty,
-        PhotoGridAdapter.MarsPropertyViewHolder>(DiffCallback) {
+@Suppress("MemberVisibilityCanBePrivate")
+class PhotoGridAdapter( private val onClickListener: OnClickListener ) :
+        ListAdapter<MarsProperty,
+                PhotoGridAdapter.MarsPropertyViewHolder>(DiffCallback) {
     /**
-     * Called when RecyclerView needs a new [ViewHolder] of the given type to represent
-     * an item.
-     *
-     *
-     * This new ViewHolder should be constructed with a new View that can represent the items
-     * of the given type. You can either create a new View manually or inflate it from an XML
-     * layout file.
-     *
-     *
-     * The new ViewHolder will be used to display items of the adapter using
-     * [.onBindViewHolder]. Since it will be re-used to display
-     * different items in the data set, it is a good idea to cache references to sub views of
-     * the View to avoid unnecessary [View.findViewById] calls.
+     * Called when RecyclerView needs a new `ViewHolder` of the given type to represent an item.
      *
      * @param parent The ViewGroup into which the new View will be added after it is bound to
      * an adapter position.
      * @param viewType The view type of the new View.
-     *
      * @return A new ViewHolder that holds a View of the given view type.
-     * @see .getItemViewType
-     * @see .onBindViewHolder
      */
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoGridAdapter.MarsPropertyViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MarsPropertyViewHolder {
         return MarsPropertyViewHolder(GridViewItemBinding.inflate(LayoutInflater.from(parent.context)))
     }
 
     /**
      * Called by RecyclerView to display the data at the specified position. This method should
-     * update the contents of the [ViewHolder.itemView] to reflect the item at the given
-     * position.
-     *
-     *
-     * Note that unlike [android.widget.ListView], RecyclerView will not call this method
-     * again if the position of the item changes in the data set unless the item itself is
-     * invalidated or the new position cannot be determined. For this reason, you should only
-     * use the `position` parameter while acquiring the related data item inside
-     * this method and should not keep a copy of it. If you need the position of an item later
-     * on (e.g. in a click listener), use [ViewHolder.getAdapterPosition] which will
-     * have the updated adapter position.
-     *
-     * Override [.onBindViewHolder] instead if Adapter can
-     * handle efficient partial bind.
+     * update the contents of the `ViewHolder` to reflect the item at the given position.
      *
      * @param holder The ViewHolder which should be updated to represent the contents of the
      * item at the given position in the data set.
@@ -77,6 +51,9 @@ class PhotoGridAdapter : ListAdapter<MarsProperty,
      */
     override fun onBindViewHolder(holder: MarsPropertyViewHolder, position: Int) {
         val marsProperty = getItem(position)
+        holder.itemView.setOnClickListener {
+            onClickListener.onClick(marsProperty)
+        }
         holder.bind(marsProperty)
     }
 
@@ -88,52 +65,41 @@ class PhotoGridAdapter : ListAdapter<MarsProperty,
         }
     }
 
+    class OnClickListener(val clickListener: (marsProperty:MarsProperty) -> Unit) {
+        fun onClick(marsProperty:MarsProperty) = clickListener(marsProperty)
+    }
+
     companion object DiffCallback : DiffUtil.ItemCallback<MarsProperty>() {
         /**
-         * Called to check whether two objects represent the same item.
+         * Called to check whether two objects represent the same item. For example, if your items
+         * have unique ids, this method should check their id equality.
          *
-         *
-         * For example, if your items have unique ids, this method should check their id equality.
-         *
-         *
-         * Note: `null` items in the list are assumed to be the same as another `null`
-         * item and are assumed to not be the same as a non-`null` item. This callback will
-         * not be invoked for either of those cases.
+         * Note: `null` items in the list are assumed to be the same as another `null` item and are
+         * assumed to not be the same as a non-`null` item. This callback will not be invoked for
+         * either of those cases.
          *
          * @param oldItem The item in the old list.
          * @param newItem The item in the new list.
-         * @return True if the two items represent the same object or false if they are different.
+         * @return *true* if the two items represent the same object or *false* if they are different.
          */
         override fun areItemsTheSame(oldItem: MarsProperty, newItem: MarsProperty): Boolean {
             return oldItem === newItem
         }
 
         /**
-         * Called to check whether two items have the same data.
+         * Called to check whether two items have the same data. This information is used to detect
+         * if the contents of an item have changed. This method to check equality instead of
+         * [Object.equals] so that you can change its behavior depending on your UI. For example,
+         * if you are using DiffUtil with a [RecyclerView.Adapter], you should return whether the
+         * items' visual representations are the same. This method is called only if [areItemsTheSame]
+         * returns `true` for these items.
          *
-         *
-         * This information is used to detect if the contents of an item have changed.
-         *
-         *
-         * This method to check equality instead of [Object.equals] so that you can
-         * change its behavior depending on your UI.
-         *
-         *
-         * For example, if you are using DiffUtil with a
-         * [RecyclerView.Adapter], you should
-         * return whether the items' visual representations are the same.
-         *
-         *
-         * This method is called only if [.areItemsTheSame] returns `true` for
-         * these items.
-         *
-         *
-         * Note: Two `null` items are assumed to represent the same contents. This callback
-         * will not be invoked for this case.
+         * Note: Two `null` items are assumed to represent the same contents. This callback will not
+         * be invoked for this case.
          *
          * @param oldItem The item in the old list.
          * @param newItem The item in the new list.
-         * @return True if the contents of the items are the same or false if they are different.
+         * @return *true* if the contents of the items are the same or *false* if they are different.
          */
         override fun areContentsTheSame(oldItem: MarsProperty, newItem: MarsProperty): Boolean {
             return oldItem.id == newItem.id
