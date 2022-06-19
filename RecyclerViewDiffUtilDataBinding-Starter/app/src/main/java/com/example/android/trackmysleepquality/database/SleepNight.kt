d52e38aa01002abc182f57d187347a9bdeb0d083
markgray <mark.t.gray@gmail.com>
2022-06-19 05:47:23 -0400

    Updated gradle dependencies and kotlin to 1.7.0 in RecyclerViewDiffUtilDataBinding-Starter

--- RecyclerViewDiffUtilDataBinding-Starter/app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
+++ RecyclerViewDiffUtilDataBinding-Starter/app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
@@ -29,7 +29,7 @@ data class SleepNight(
         var nightId: Long = 0L,
 
         @ColumnInfo(name = "start_time_milli")
-        val startTimeMilli: Long = System.currentTimeMillis(),
+        var startTimeMilli: Long = System.currentTimeMillis(),
 
         @ColumnInfo(name = "end_time_milli")
         var endTimeMilli: Long = startTimeMilli,
