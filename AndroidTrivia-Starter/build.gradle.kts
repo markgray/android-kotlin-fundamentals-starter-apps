2ae1d43ca50ce8d397681945ff8be7052ac24f42
markgray <mark.t.gray@gmail.com>
2024-06-11 06:28:44 -0400

    Converted build files to kts in AndroidTrivia-Starter

--- AndroidTrivia-Starter/build.gradle.kts
+++ AndroidTrivia-Starter/build.gradle.kts
@@ -14,4 +14,10 @@
  * limitations under the License.
  */
 
-include ':app'
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+plugins {
+    id("com.android.application") version "8.4.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("androidx.navigation.safeargs") version "2.7.7" apply false
+}
