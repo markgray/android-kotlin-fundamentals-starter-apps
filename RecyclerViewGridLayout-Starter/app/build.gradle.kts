83c9019bc73959ed0fc15a20db6f2ba17a5a615a
markgray <mark.t.gray@gmail.com>
2024-06-13 06:39:31 -0400

    Updated gradle dependency in RecyclerViewGridLayout-Starter

--- RecyclerViewGridLayout-Starter/app/build.gradle.kts
+++ RecyclerViewGridLayout-Starter/app/build.gradle.kts
@@ -58,7 +58,7 @@ android {
 dependencies {
     // Support libraries
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.fragment:fragment-ktx:1.7.1")
+    implementation("androidx.fragment:fragment-ktx:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
 
