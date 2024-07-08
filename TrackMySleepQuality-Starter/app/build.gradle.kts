b36824d5eba8b870ce82179f503d5238bbc7e1af
Mark Gray <mark.t.gray@gmail.com>
2024-07-08 04:54:53 -0400

    Updated kotlin to 2.0.0, and updated gradle dependencies in TrackMySleepQuality-Starter

--- TrackMySleepQuality-Starter/app/build.gradle.kts
+++ TrackMySleepQuality-Starter/app/build.gradle.kts
@@ -60,7 +60,7 @@ android {
 dependencies {
     // Support libraries
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.fragment:fragment-ktx:1.8.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     // Android KTX
@@ -81,6 +81,6 @@ dependencies {
 
     // Testing
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
