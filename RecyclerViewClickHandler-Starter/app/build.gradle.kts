9133af7532ad56a21a8e67696fabde0136697df4
Mark Gray <mark.t.gray@gmail.com>
2024-07-08 04:17:54 -0400

    Updated kotlin to 2.0.0, and updated gradle dependencies in RecyclerViewClickHandler-Starter

--- RecyclerViewClickHandler-Starter/app/build.gradle.kts
+++ RecyclerViewClickHandler-Starter/app/build.gradle.kts
@@ -60,7 +60,7 @@ android {
 dependencies {
     // Support libraries
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.fragment:fragment-ktx:1.8.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     // Android KTX
@@ -82,6 +82,6 @@ dependencies {
 
     // Testing
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test:runner:1.5.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test:runner:1.6.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
