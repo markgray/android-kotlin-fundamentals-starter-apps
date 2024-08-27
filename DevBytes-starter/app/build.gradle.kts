18b5b9b7c7fb5431f71a8cc9a1eded4751860b5b
markgray <mark.t.gray@gmail.com>
2024-08-27 05:36:33 -0400

    Updated kotlin to 2.0.20, updated ksp to 2.0.20-1.0.24, and updated gradle dependency in DevBytes-starter

--- DevBytes-starter/app/build.gradle.kts
+++ DevBytes-starter/app/build.gradle.kts
@@ -58,7 +58,7 @@ android {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-reflect:2.0.0")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:2.0.20")
 
     // support libraries
     implementation("androidx.appcompat:appcompat:1.7.0")
