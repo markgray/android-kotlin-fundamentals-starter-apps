0a6f78d881ec3884c5e78eee5b4b69283477356e
markgray <mark.t.gray@gmail.com>
2024-08-27 05:47:47 -0400

    Updated kotlin to 2.0.20, and updated parcelize to 2.0.20 in MarsRealEstate-Starter

--- MarsRealEstate-Starter/build.gradle.kts
+++ MarsRealEstate-Starter/build.gradle.kts
@@ -2,7 +2,7 @@
 
 plugins {
     id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.plugin.parcelize") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+    id("org.jetbrains.kotlin.plugin.parcelize") version "2.0.20" apply false
     id("androidx.navigation.safeargs") version "2.7.7" apply false
 }
