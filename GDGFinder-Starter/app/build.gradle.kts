faae74a94217a857d81638c8a4662aea4a80c407
Mark Gray <mark.t.gray@gmail.com>
2024-09-12 03:17:56 -0400

    Updated gradle dependencies in GDGFinder-Starter

--- GDGFinder-Starter/app/build.gradle.kts
+++ GDGFinder-Starter/app/build.gradle.kts
@@ -64,8 +64,8 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     // Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.0")
 
     // Core with Ktx
     implementation("androidx.core:core-ktx:1.13.1")
@@ -83,7 +83,7 @@ dependencies {
     // ViewModel and LiveData
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
     // use viewModelScope from lifecycle-viewmodel-ktx
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.5")
 
     // Moshi
     implementation("com.squareup.moshi:moshi:1.15.1")
