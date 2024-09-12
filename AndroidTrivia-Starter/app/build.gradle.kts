518b549578553bd0a1e247afdfd1eb8ff962bed9
Mark Gray <mark.t.gray@gmail.com>
2024-09-12 03:07:33 -0400

    Updated gradle dependencies in AndroidTrivia-Starter

--- AndroidTrivia-Starter/app/build.gradle.kts
+++ AndroidTrivia-Starter/app/build.gradle.kts
@@ -64,8 +64,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test:runner:1.6.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    implementation("androidx.navigation:navigation-runtime-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-runtime-ktx:2.8.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.0")
     implementation("com.google.android.material:material:1.12.0")
 }
