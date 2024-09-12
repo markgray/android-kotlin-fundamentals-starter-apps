a7a748125de33de104a5afa8ee58f967b8c122d0
Mark Gray <mark.t.gray@gmail.com>
2024-09-12 04:20:33 -0400

    Updated gradle dependencies in TrackMySleepQuality-Starter

--- TrackMySleepQuality-Starter/app/build.gradle.kts
+++ TrackMySleepQuality-Starter/app/build.gradle.kts
@@ -60,7 +60,7 @@ android {
 dependencies {
     // Support libraries
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.fragment:fragment-ktx:1.8.2")
+    implementation("androidx.fragment:fragment-ktx:1.8.3")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     // Android KTX
@@ -70,8 +70,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.0")
 
     // Room and Lifecycle dependencies
     implementation("androidx.room:room-runtime:2.6.1")
