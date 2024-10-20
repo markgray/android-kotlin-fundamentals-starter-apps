a39d9102740ac5640f45c28bbc7ef784c40feb05
markgray <mark.t.gray@gmail.com>
2024-10-20 05:12:33 -0400

    Updated AGP to 8.7.1, and updated gradle dependency in AboutMeDataBinding-Starter

--- AboutMeDataBinding-Starter/app/build.gradle.kts
+++ AboutMeDataBinding-Starter/app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.9.2")
+    implementation("androidx.activity:activity-ktx:1.9.3")
     implementation("androidx.appcompat:appcompat:1.7.0")
     testImplementation("junit:junit:4.13.2")
 }
