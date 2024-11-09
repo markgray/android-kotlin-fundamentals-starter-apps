21f365940c0ceed0b76ee157d1ec75636090d356
markgray <mark.t.gray@gmail.com>
2024-11-09 05:57:27 -0500

    Updated AGP to 8.7.2 and updated gradle dependencies in AboutMeInteractive-Starter

--- AboutMeInteractive-Starter/app/build.gradle.kts
+++ AboutMeInteractive-Starter/app/build.gradle.kts
@@ -51,6 +51,6 @@ android {
 dependencies {
     implementation("androidx.activity:activity-ktx:1.9.3")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
 }
