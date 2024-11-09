9f334130496dc60eab2e65142ab79d1de3b2e544
markgray <mark.t.gray@gmail.com>
2024-11-09 06:31:38 -0500

    Updated AGP to 8.7.2 and updated gradle dependencies in DevBytes-starter

--- DevBytes-starter/app/build.gradle.kts
+++ DevBytes-starter/app/build.gradle.kts
@@ -67,10 +67,10 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
 
     // Android KTX
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core-ktx:1.15.0")
 
     // constraint layout
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
 
     // navigation
     implementation("androidx.navigation:navigation-runtime-ktx:2.8.3")
@@ -110,5 +110,5 @@ dependencies {
     ksp("androidx.room:room-compiler:2.6.1")
 
     // Work dependency
-    implementation("androidx.work:work-runtime-ktx:2.9.1")
+    implementation("androidx.work:work-runtime-ktx:2.10.0")
 }
