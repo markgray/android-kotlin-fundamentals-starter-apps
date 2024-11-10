bddfa806a4406224938c16e56b3d28eeca95a21d
markgray <mark.t.gray@gmail.com>
2024-11-10 05:02:56 -0500

    Updated AGP to 8.7.2 and updated gradle dependencies in RecyclerViewDiffUtilDataBinding-Starter

--- RecyclerViewDiffUtilDataBinding-Starter/app/build.gradle.kts
+++ RecyclerViewDiffUtilDataBinding-Starter/app/build.gradle.kts
@@ -60,12 +60,12 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.9.3")
     // Support libraries
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.fragment:fragment-ktx:1.8.4")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.fragment:fragment-ktx:1.8.5")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
 
     // Android KTX
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core-ktx:1.15.0")
 
     // Navigation
     implementation("androidx.navigation:navigation-fragment-ktx:2.8.3")
