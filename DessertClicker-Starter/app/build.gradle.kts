cca3f7f9ed35c7c6b32ebb1aaf11a9f6730bc473
markgray <mark.t.gray@gmail.com>
2025-01-27 06:55:24 -0500

    Updated gradle dependency in DessertClicker-Starter

--- DessertClicker-Starter/app/build.gradle.kts
+++ DessertClicker-Starter/app/build.gradle.kts
@@ -55,7 +55,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.9.3")
+    implementation("androidx.activity:activity-ktx:1.10.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     implementation("com.jakewharton.timber:timber:5.0.1")
