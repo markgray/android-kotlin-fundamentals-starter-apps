18d924ba0450af8c92f845142e87b8a0a3093afa
Mark Gray <mark.t.gray@gmail.com>
2025-01-28 04:29:55 -0500

    Updated gradle dependencies in RecyclerViewDiffUtilDataBinding-Starter

--- RecyclerViewDiffUtilDataBinding-Starter/app/build.gradle.kts
+++ RecyclerViewDiffUtilDataBinding-Starter/app/build.gradle.kts
@@ -57,12 +57,12 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.9.3")
+    implementation("androidx.activity:activity-ktx:1.10.0")
     // Support libraries
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.fragment:fragment-ktx:1.8.5")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
 
     // Android KTX
     implementation("androidx.core:core-ktx:1.15.0")
