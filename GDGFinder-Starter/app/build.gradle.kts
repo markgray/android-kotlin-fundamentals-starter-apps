3ae6ba8f915e887a60f776d028a3f803b235137b
Mark Gray <mark.t.gray@gmail.com>
2025-01-28 04:04:52 -0500

    Updated gradle dependencies in GDGFinder-Starter

--- GDGFinder-Starter/app/build.gradle.kts
+++ GDGFinder-Starter/app/build.gradle.kts
@@ -58,7 +58,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.9.3")
+    implementation("androidx.activity:activity-ktx:1.10.0")
     // Kotlin
 
     // Constraint Layout
@@ -72,7 +72,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
 
     // RecyclerView
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
 
     // material design components
     implementation("com.google.android.material:material:1.12.0")
