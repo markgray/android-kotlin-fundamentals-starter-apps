ff0c8ac259beacd9075b8fb1b6d8adf860bd48f7
Mark Gray <mark.t.gray@gmail.com>
2025-02-10 04:56:31 -0500

    Updated kotlin to 2.1.10 and updated gradle dependencies in GDGFinder-Starter

--- GDGFinder-Starter/build.gradle.kts
+++ GDGFinder-Starter/build.gradle.kts
@@ -2,7 +2,7 @@
 
 plugins {
     id("com.android.application") version "8.8.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("org.jetbrains.kotlin.plugin.parcelize") version "2.1.0" apply false
-    id("androidx.navigation.safeargs") version "2.8.5" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
+    id("org.jetbrains.kotlin.plugin.parcelize") version "2.1.10" apply false
+    id("androidx.navigation.safeargs") version "2.8.6" apply false
 }
