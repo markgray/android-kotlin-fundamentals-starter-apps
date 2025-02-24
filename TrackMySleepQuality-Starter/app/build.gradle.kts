943a88aee540550dce2ccd5f32335fac6c936060
Mark Gray <mark.t.gray@gmail.com>
2025-02-24 04:25:38 -0500

    Updated AGP to 8.8.1 and updated gradle dependencies in TrackMySleepQuality-Starter

--- TrackMySleepQuality-Starter/app/build.gradle.kts
+++ TrackMySleepQuality-Starter/app/build.gradle.kts
@@ -61,18 +61,18 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.10.0")
     // Support libraries
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.fragment:fragment-ktx:1.8.5")
+    implementation("androidx.fragment:fragment-ktx:1.8.6")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
 
     // Android KTX
     implementation("androidx.core:core-ktx:1.15.0")
 
     // Co-routines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.6")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.6")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.7")
 
     // Room and Lifecycle dependencies
     implementation("androidx.room:room-runtime:2.6.1")
