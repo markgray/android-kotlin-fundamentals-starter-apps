fe8db573464c42e66ccd84191ca351516e6cbff4
Mark Gray <mark.t.gray@gmail.com>
2025-03-09 04:18:52 -0400

    Updated AGP to 8.9.0 and updated gradle dependencies in AndroidTrivia-Starter

--- AndroidTrivia-Starter/app/build.gradle.kts
+++ AndroidTrivia-Starter/app/build.gradle.kts
@@ -57,16 +57,16 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.10.0")
+    implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("org.jetbrains.kotlin:kotlin-reflect:2.1.10")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test:runner:1.6.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    implementation("androidx.navigation:navigation-runtime-ktx:2.8.7")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.7")
+    implementation("androidx.navigation:navigation-runtime-ktx:2.8.8")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.8")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.8")
     implementation("com.google.android.material:material:1.12.0")
 }
