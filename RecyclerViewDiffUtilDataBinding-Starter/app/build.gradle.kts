987afd0bdbfb46e59728c363d1571cf0870ac378
Mark Gray <mark.t.gray@gmail.com>
2025-03-09 05:13:51 -0400

    Updated AGP to 8.9.0 and updated gradle dependencies in RecyclerViewDiffUtilDataBinding-Starter

--- RecyclerViewDiffUtilDataBinding-Starter/app/build.gradle.kts
+++ RecyclerViewDiffUtilDataBinding-Starter/app/build.gradle.kts
@@ -57,23 +57,23 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.10.0")
+    implementation("androidx.activity:activity-ktx:1.10.1")
     // Support libraries
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.fragment:fragment-ktx:1.8.5")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.6")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
 
     // Android KTX
     implementation("androidx.core:core-ktx:1.15.0")
 
     // Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.6")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.6")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.8")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.8")
 
     // Co-routines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
 
     // Room and Lifecycle dependencies
     implementation("androidx.room:room-runtime:2.6.1")
