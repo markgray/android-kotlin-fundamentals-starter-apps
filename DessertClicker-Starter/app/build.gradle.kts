a42d0723a79f468fa4824e57de4e0fdd1454a81f
Mark Gray <mark.t.gray@gmail.com>
2025-03-09 04:32:42 -0400

    Updated AGP to 8.9.0 and updated gradle dependencies in DessertClicker-Starter

--- DessertClicker-Starter/app/build.gradle.kts
+++ DessertClicker-Starter/app/build.gradle.kts
@@ -55,8 +55,8 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.10.0")
+    implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("com.jakewharton.timber:timber:5.0.1")
 }
