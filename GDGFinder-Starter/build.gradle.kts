e6e5e8603d65f4db283f56acbdf6cdf9c1721378
Mark Gray <mark.t.gray@gmail.com>
2025-03-20 03:33:27 -0400

    Updated gradle dependencies in GDGFinder-Starter

--- GDGFinder-Starter/build.gradle.kts
+++ GDGFinder-Starter/build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
     id("com.android.application") version "8.9.0" apply false
     id("org.jetbrains.kotlin.android") version "2.1.10" apply false
     id("org.jetbrains.kotlin.plugin.parcelize") version "2.1.10" apply false
-    id("androidx.navigation.safeargs") version "2.8.8" apply false
+    id("androidx.navigation.safeargs") version "2.8.9" apply false
 }
