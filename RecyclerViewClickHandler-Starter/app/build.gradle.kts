95601998c27c1562f563c19be09c21e8f5e1c617
Mark Gray <mark.t.gray@gmail.com>
2025-05-03 04:08:18 -0400

    Updated gradle dependencies in RecyclerViewClickHandler-Starter

--- RecyclerViewClickHandler-Starter/app/build.gradle.kts
+++ RecyclerViewClickHandler-Starter/app/build.gradle.kts
@@ -68,8 +68,8 @@ dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
 
     // Room and Lifecycle dependencies
-    implementation("androidx.room:room-runtime:2.7.0")
-    ksp("androidx.room:room-compiler:2.7.0")
+    implementation("androidx.room:room-runtime:2.7.1")
+    ksp("androidx.room:room-compiler:2.7.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
 
