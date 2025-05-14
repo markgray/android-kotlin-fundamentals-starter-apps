aa88aaf6b022d08b4d9164febf6e086103518584
Mark Gray <mark.t.gray@gmail.com>
2025-05-14 04:47:11 -0400

    Updated AGP to 8.10.0, updated gradle to 8.14, updated kotlin to 2.1.21 and updated gradle dependencies in GuessTheWord-Starter

--- GuessTheWord-Starter/app/build.gradle.kts
+++ GuessTheWord-Starter/app/build.gradle.kts
@@ -66,8 +66,8 @@ dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
 
     // Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.9")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.9")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.9.0")
 
     //ViewModel and LiveData
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
