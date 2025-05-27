c609808906f06ec39648508d9aba219c130b7956
markgray <mark.t.gray@gmail.com>
2025-05-27 06:04:21 -0400

    Updated gradle dependency in RecyclerViewClickHandler-Starter

--- RecyclerViewClickHandler-Starter/app/build.gradle.kts
+++ RecyclerViewClickHandler-Starter/app/build.gradle.kts
@@ -61,7 +61,7 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.10.1")
     // Support libraries
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.fragment:fragment-ktx:1.8.6")
+    implementation("androidx.fragment:fragment-ktx:1.8.7")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
 
     // Android KTX
