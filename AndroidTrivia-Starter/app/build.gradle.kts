27297ede4dc2c4253a0d2ae3052ee60246611475
markgray <mark.t.gray@gmail.com>
2025-06-10 07:16:52 -0400

    Updated AGP to 8.10.1, and updated gradle to 8.14.1 and updated gradle dependency in AndroidTrivia-Starter

--- AndroidTrivia-Starter/app/build.gradle.kts
+++ AndroidTrivia-Starter/app/build.gradle.kts
@@ -59,7 +59,7 @@ android {
 dependencies {
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("org.jetbrains.kotlin:kotlin-reflect:2.1.21")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     testImplementation("junit:junit:4.13.2")
