ee9e021a613e532414b26877e88890fe05eec698
Mark Gray <mark.t.gray@gmail.com>
2025-06-11 03:39:08 -0400

    Updated AGP to 8.10.1, and updated gradle to 8.14.2 and updated gradle dependency in GDGFinder-Starter

--- GDGFinder-Starter/app/build.gradle.kts
+++ GDGFinder-Starter/app/build.gradle.kts
@@ -84,7 +84,7 @@ dependencies {
     // ViewModel and LiveData
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
     // use viewModelScope from lifecycle-viewmodel-ktx
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.1")
 
     // Moshi
     implementation("com.squareup.moshi:moshi:1.15.1")
