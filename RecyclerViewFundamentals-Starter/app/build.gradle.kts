f823e024750d5b9a750264f8c53fa9e2618ec842
Mark Gray <mark.t.gray@gmail.com>
2025-06-29 03:11:57 -0400

    Updated AGP to 8.11.0, updated kotlin to 2.2.0, switched to "kotlin compilerOptions" and updated gradle dependencies in RecyclerViewFundamentals-Starter

--- RecyclerViewFundamentals-Starter/app/build.gradle.kts
+++ RecyclerViewFundamentals-Starter/app/build.gradle.kts
@@ -1,18 +1,4 @@
-/*
- * Copyright 2019, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     id("com.android.application")
@@ -23,11 +9,11 @@ plugins {
 }
 
 android {
-    compileSdk = 35
+    compileSdk = 36
     defaultConfig {
         applicationId = "com.example.android.trackmysleepquality"
         minSdk = 21
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -50,8 +36,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_17
+        }
     }
     namespace = "com.example.android.trackmysleepquality"
 }
@@ -76,9 +64,9 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.9.0")
 
     // Room and Lifecycle dependencies
-    implementation("androidx.room:room-runtime:2.7.1")
+    implementation("androidx.room:room-runtime:2.7.2")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    ksp("androidx.room:room-compiler:2.7.1")
+    ksp("androidx.room:room-compiler:2.7.2")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
 
     // Testing
