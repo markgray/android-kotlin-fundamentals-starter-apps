533a97f9b6595ca9c9c1bbfbda7e1a921a64c680
Mark Gray <mark.t.gray@gmail.com>
2025-07-13 04:28:47 -0400

    Updated AGP to 8.11.1, updated gradle to 8.14.3, and updated gradle dependencies in TrackMySleepQuality-Starter

--- TrackMySleepQuality-Starter/app/build.gradle.kts
+++ TrackMySleepQuality-Starter/app/build.gradle.kts
@@ -59,8 +59,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.9.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.9.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.1")
+    implementation("androidx.navigation:navigation-ui-ktx:2.9.1")
 
     // Room and Lifecycle dependencies
     implementation("androidx.room:room-runtime:2.7.2")
