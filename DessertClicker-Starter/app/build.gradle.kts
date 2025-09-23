7004e640ee54360c78ac62949a2e2f68e6795afb
Mark Gray <mark.t.gray@gmail.com>
2025-09-23 03:45:33 -0400

    Updated kotlin to 2.2.20, fixed warnings, and updated gradle dependencies in DessertClicker-Starter

--- DessertClicker-Starter/app/build.gradle.kts
+++ DessertClicker-Starter/app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.10.1")
+    implementation("androidx.activity:activity-ktx:1.11.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("com.jakewharton.timber:timber:5.0.1")
