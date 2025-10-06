05a8eb4da4768a014e18e1e5a08a0febf1296903
Mark Gray <mark.t.gray@gmail.com>
2025-10-06 03:50:43 -0400

    Updated gradle to 9.1.0, and updated gradle dependencies in RecyclerViewDiffUtilDataBinding-Starter

--- RecyclerViewDiffUtilDataBinding-Starter/app/build.gradle.kts
+++ RecyclerViewDiffUtilDataBinding-Starter/app/build.gradle.kts
@@ -56,17 +56,17 @@ dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
 
     // Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.9.4")
-    implementation("androidx.navigation:navigation-ui-ktx:2.9.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.9.5")
 
     // Co-routines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
 
     // Room and Lifecycle dependencies
-    implementation("androidx.room:room-runtime:2.8.0")
+    implementation("androidx.room:room-runtime:2.8.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    ksp("androidx.room:room-compiler:2.8.0")
+    ksp("androidx.room:room-compiler:2.8.1")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
 
     // Testing
