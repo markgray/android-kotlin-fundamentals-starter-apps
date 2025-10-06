e13624edc57632ae5fbc5c1507fb1891c74c6d2e
Mark Gray <mark.t.gray@gmail.com>
2025-10-06 03:46:35 -0400

    Updated gradle to 9.1.0, and updated gradle dependencies in RecyclerViewClickHandler-Starter

--- RecyclerViewClickHandler-Starter/app/build.gradle.kts
+++ RecyclerViewClickHandler-Starter/app/build.gradle.kts
@@ -56,8 +56,8 @@ dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
 
     // Room and Lifecycle dependencies
-    implementation("androidx.room:room-runtime:2.8.0")
-    ksp("androidx.room:room-compiler:2.8.0")
+    implementation("androidx.room:room-runtime:2.8.1")
+    ksp("androidx.room:room-compiler:2.8.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
 
@@ -66,8 +66,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
 
     // Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.9.4")
-    implementation("androidx.navigation:navigation-ui-ktx:2.9.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.9.5")
 
     // Testing
     testImplementation("junit:junit:4.13.2")
