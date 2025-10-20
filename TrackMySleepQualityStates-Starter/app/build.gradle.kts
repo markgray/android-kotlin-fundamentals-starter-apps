435d71e388f1c34c5a7b1af9603ebabef9be6ab6
markgray <mark.t.gray@gmail.com>
2025-10-20 05:19:26 -0400

    Updated date of gradle-wrapper.properties, updated gradle dependencies, and added signingConfig to release variant in TrackMySleepQualityStates-Starter

--- TrackMySleepQualityStates-Starter/app/build.gradle.kts
+++ TrackMySleepQualityStates-Starter/app/build.gradle.kts
@@ -23,9 +23,10 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(
-                getDefaultProguardFile("proguard-android.txt"),
+                getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     buildFeatures {
@@ -54,9 +55,9 @@ dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
 
     // Room and Lifecycle dependencies
-    implementation("androidx.room:room-runtime:2.8.1")
+    implementation("androidx.room:room-runtime:2.8.2")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    ksp("androidx.room:room-compiler:2.8.1")
+    ksp("androidx.room:room-compiler:2.8.2")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
 
     // Co-routines
