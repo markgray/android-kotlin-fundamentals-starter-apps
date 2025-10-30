73ded1bbac0ba325cb7f89a3c43ae71f11a9c275
Mark Gray <mark.t.gray@gmail.com>
2025-10-30 03:55:14 -0400

    Updated kotlin to 2.2.21 and updated gradle dependencies in DevBytes-starter

--- DevBytes-starter/app/build.gradle.kts
+++ DevBytes-starter/app/build.gradle.kts
@@ -48,7 +48,7 @@ android {
 
 dependencies {
     implementation("androidx.activity:activity-ktx:1.11.0")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:2.2.20")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:2.2.21")
 
     // support libraries
     implementation("androidx.appcompat:appcompat:1.7.1")
@@ -95,9 +95,9 @@ dependencies {
     ksp("com.github.bumptech.glide:compiler:5.0.5")
 
     // Room dependency
-    implementation("androidx.room:room-runtime:2.8.2")
-    ksp("androidx.room:room-compiler:2.8.2")
+    implementation("androidx.room:room-runtime:2.8.3")
+    ksp("androidx.room:room-compiler:2.8.3")
 
     // Work dependency
-    implementation("androidx.work:work-runtime-ktx:2.10.5")
+    implementation("androidx.work:work-runtime-ktx:2.11.0")
 }
