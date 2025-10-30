e81a44a76635941e1be0190e2b967e5ad8b07052
markgray <mark.t.gray@gmail.com>
2025-10-30 06:21:38 -0400

    Updated kotlin to 2.2.21 and updated gradle dependencies in TrackMySleepQualityStates-Starter

--- TrackMySleepQualityStates-Starter/app/build.gradle.kts
+++ TrackMySleepQualityStates-Starter/app/build.gradle.kts
@@ -55,9 +55,9 @@ dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
 
     // Room and Lifecycle dependencies
-    implementation("androidx.room:room-runtime:2.8.2")
+    implementation("androidx.room:room-runtime:2.8.3")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    ksp("androidx.room:room-compiler:2.8.2")
+    ksp("androidx.room:room-compiler:2.8.3")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
 
     // Co-routines
