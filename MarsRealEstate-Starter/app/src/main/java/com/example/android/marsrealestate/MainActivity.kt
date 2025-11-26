c66a98db1a359d0cf2857d75c892041679a6c0f1
markgray <mark.t.gray@gmail.com>
2025-11-26 03:53:53 -0500

    Added comments in MarsRealEstate-Starter

--- MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/MainActivity.kt
+++ MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/MainActivity.kt
@@ -18,13 +18,16 @@
 package com.example.android.marsrealestate
 
 import android.os.Bundle
+import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.fragment.NavHostFragment
 
 /**
  * Our MainActivity is only responsible for setting the content view that contains the
@@ -33,14 +36,29 @@ import androidx.fragment.app.FragmentContainerView
 class MainActivity : AppCompatActivity() {
 
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
-     * then we set our content view to our layout file `R.layout.activity_main`. It consists only
-     * of a `fragment` container for android:name="androidx.navigation.fragment.NavHostFragment",
-     * with an app:defaultNavHost="true" attribute (to ensure that our `NavHostFragment` intercepts
-     * the system Back button) and an app:navGraph="@navigation/nav_graph" attribute (associates the
-     * `NavHostFragment` with the navigation graph navigation/nav_graph.xml). The app:startDestination
-     * attribute of the navigation graph specifies `R.id.overviewFragment` to be the starting
-     * fragment (`overview/OverviewFragment.kt`)
+     * Called when the activity is starting. First we call [enableEdgeToEdge] to enable edge to edge
+     * display then we call our super's implementation of `onCreate`, and set our content view to
+     * our layout file `R.layout.activity_main`. It consists only of a `fragment` container for
+     * android:name="androidx.navigation.fragment.NavHostFragment", with an app:defaultNavHost="true"
+     * attribute (to ensure that our `NavHostFragment` intercepts the system Back button) and an
+     * app:navGraph="@navigation/nav_graph" attribute (associates the [NavHostFragment] with the
+     * navigation graph navigation/nav_graph.xml). The app:startDestination attribute of the
+     * navigation graph specifies `R.id.overviewFragment` to be the starting fragment
+     * (`overview/OverviewFragment.kt`).
+     *
+     * We initialize our [FragmentContainerView] variable `rootView` to the view with
+     * ID `R.id.nav_host_fragment` then call [ViewCompat.setOnApplyWindowInsetsListener]
+     * to take over the policy for applying window insets to `rootView`, with the listener
+     * argument a lambda that accepts the [View] passed the lambda in variable `v` and
+     * the [WindowInsetsCompat] passed the lambda in variable `windowInsets`. It
+     * initializes its [Insets] variable `insets` to the [WindowInsetsCompat.getInsets]
+     * of `windowInsets` with [WindowInsetsCompat.Type.systemBars] as the
+     * argument, then it updates the layout parameters of `v` to be a
+     * [ViewGroup.MarginLayoutParams] with the left margin set to `insets.left`,
+     * the right margin set to `insets.right`, the top margin set to `insets.top`,
+     * and the bottom margin set to `insets.bottom`. Finally it returns
+     * [WindowInsetsCompat.CONSUMED] to the caller (so that the window insets
+     * will not keep passing down to descendant views).
      *
      * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
@@ -49,8 +67,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rootView = findViewById<FragmentContainerView>(R.id.nav_host_fragment)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
