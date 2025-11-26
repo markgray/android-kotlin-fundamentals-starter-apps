18e4fa5e66ef297735da858b3b77a08a5f6639d7
markgray <mark.t.gray@gmail.com>
2025-11-26 03:29:38 -0500

    Added comments in DevBytes-starter

--- DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/ui/DevByteActivity.kt
+++ DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/ui/DevByteActivity.kt
@@ -17,11 +17,14 @@
 package com.example.android.devbyteviewer.ui
 
 import android.os.Bundle
+import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
+import androidx.core.view.OnApplyWindowInsetsListener
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
 import com.example.android.devbyteviewer.R
@@ -33,8 +36,24 @@ import com.example.android.devbyteviewer.R
 class DevByteActivity : AppCompatActivity() {
 
     /**
-     * Called when the activity is starting.  This is where most initialization
-     * should go
+     * Called when the activity is starting. This is where most initialization should go. First we
+     * call [enableEdgeToEdge] to enable the edge-to-edge display. Then we call our super's
+     * implementation of `onCreate`, and set our content view to our layout file
+     * `R.layout.activity_dev_byte_viewer`.
+     *
+     * We initialize our [FrameLayout] variable `rootView` to the view with ID `R.id.root_view`,
+     * then we call the [ViewCompat.setOnApplyWindowInsetsListener] method to set a
+     * [OnApplyWindowInsetsListener] to take over over the policy for applying window insets to
+     * `rootView`, with the `listener` argument a lambda that accepts the [View] passed the lambda
+     * in variable `v` and the [WindowInsetsCompat] passed the lambda in variable `windowInsets`.
+     * It initializes its [Insets] variable `insets` to the [WindowInsetsCompat.getInsets] of
+     * `windowInsets` with [WindowInsetsCompat.Type.systemBars] as the argument, then it updates the
+     * layout parameters of `v` to be a [ViewGroup.MarginLayoutParams] with the left margin set to
+     * `insets.left`, the right margin set to `insets.right`, the top margin set to `insets.top`,
+     * and the bottom margin set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to descendant views).
+     *
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
@@ -42,8 +61,8 @@ class DevByteActivity : AppCompatActivity() {
         setContentView(R.layout.activity_dev_byte_viewer)
 
         val rootView = findViewById<FrameLayout>(R.id.root_view)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
