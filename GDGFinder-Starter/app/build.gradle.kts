1faa95e4d8902ceefda2d9aff0caa58b48a0db16
markgray <mark.t.gray@gmail.com>
2025-12-07 07:09:11 -0500

    Updated gradle dependencies in GDGFinder-Starter

--- GDGFinder-Starter/app/build.gradle.kts
+++ GDGFinder-Starter/app/build.gradle.kts
@@ -46,7 +46,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.12.0")
+    implementation("androidx.activity:activity-ktx:1.12.1")
     // Kotlin
 
     // Constraint Layout
