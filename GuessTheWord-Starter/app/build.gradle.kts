65c5c1c31c1261596d06b15601ccafb41907140f
markgray <mark.t.gray@gmail.com>
2025-12-07 07:16:56 -0500

    Updated gradle dependencies in GuessTheWord-Starter

--- GuessTheWord-Starter/app/build.gradle.kts
+++ GuessTheWord-Starter/app/build.gradle.kts
@@ -45,7 +45,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.12.0")
+    implementation("androidx.activity:activity-ktx:1.12.1")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
