f05e09e0439e9641ab80b81c8dc336f2fae60dfc
Mark Gray <mark.t.gray@gmail.com>
2025-12-19 03:22:18 -0500

    Updated kotlin to 2.3.0 and updated gradle dependency in AboutMeDataBinding-Starter

--- AboutMeDataBinding-Starter/app/build.gradle.kts
+++ AboutMeDataBinding-Starter/app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.12.1")
+    implementation("androidx.activity:activity-ktx:1.12.2")
     implementation("androidx.appcompat:appcompat:1.7.1")
     testImplementation("junit:junit:4.13.2")
 }
